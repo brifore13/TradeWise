299932d19dcb20571b8f43721062dd243eb0641b
BriAnna Foreman <briforeman3@gmail.com>
2025-02-06 15:19:58 -0800

    pages loaded to react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import LoginPage from './components/auth/LoginPage'
+import Dashboard from './components/dashboard/Dashboard'
+import MarketView from './components/market/MarketView'
+import TradingView from './components/trading/TradingView'
+import PortfolioView from './components/portfolio/PorfolioView'
+import './app.css';
 
 function App() {
   return (
     <Router> 
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/market" element={<MarketView />} />
+        <Route path="/trading" element={<TradingView />} />
+        <Route path="/portfolio" element={<PortfolioView />} />
       </Routes>
     </Router>
   );
